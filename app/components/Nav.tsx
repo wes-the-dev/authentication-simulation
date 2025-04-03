b0d18d603251775ea12788e64763d6862b97b820
Wesley Chigbu <chigbuw@yahoo.com>
2025-04-03 16:44:52 +0100

    back to home link

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+"use client"
+import { usePathname } from 'next/navigation';
 import Link from "next/link";
 import {SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs"
 
 const Nav = () => {
+  const locationPath = usePathname();
+  const userProfilePath = locationPath ==='/user-profile'
   return (
     <nav className="flex gap-3 justify-end w-[100%] p-5">
         <SignedOut>
             <SignInButton mode='modal' />
         </SignedOut>
         <SignedIn>
+          {
+            userProfilePath ? 
+            <Link href='/'>
+              Back Home
+            </Link>
+            : ''
+          }
+            
             <Link href='/user-profile'>
                 Profile
             </Link>
